3492f74da2b18ba6e04e77ca19f08b2f2004d133
emrecanayar <emrecan.ayar@technoface.com.tr>
2024-07-30 11:14:06 +0300

    Sliding Expiration Caching

---
+++ SlidingExpiration/Controllers/DataController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SlidingExpiration.Services;
+
+namespace SlidingExpiration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+        private readonly DataService _dataService;
+
+        public DataController(DataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        [HttpGet("{key}")]
+        public IActionResult Get(string key)
+        {
+            var data = _dataService.GetDate(key);
+            return Ok(data);
+        }
+    }
+}
---
+++ SlidingExpiration/Services/DataService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SlidingExpiration.Services
+{
+    public class DataService
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public DataService(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public string GetDate(string key)
+        {
+            if (!_memoryCache.TryGetValue(key, out string cachedData))
+            {
+                // Öğe önbellekte yoksa, veriyi alın ve önbelleğe ekleyin
+                cachedData = $"Data for {key} - {DateTime.Now}";
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(5)) // 5 dakikalık kaydırmalı sona erme süresi
+                    .SetAbsoluteExpiration(TimeSpan.FromSeconds(10)) // 1 saatlik mutlak sona erme süresi
+                    .SetPriority(CacheItemPriority.Normal)  // Normal öncelikli önbellek öğesi
+                    .SetSize(1); ; // Boyutunu belirleyin (MemoryCache'in boyut sınırı varsa kullanışlı)
+
+                _memoryCache.Set(key, cachedData, cacheEntryOptions);
+            }
+
+            return cachedData;
+        }
+    }
+}
+
+
